d69ddbe2f70e9687c2bb34d9370a52696ad7848f
Alen Reuel <125646791+freezeknife@users.noreply.github.com>
2023-06-08 20:57:18 +0800

    Add files via upload

---
+++ ChatGPT Sentiment Analysis/contact_sheet.py
@@ -0,0 +1,56 @@
+# obtained from https://code.activestate.com/recipes/412982-use-pil-to-make-a-contact-sheet-montage-of-images/
+from PIL import Image
+
+def make_contact_sheet(fnames, ncols,nrows, photow,photoh, marl,mart,marr,marb, padding):
+    """\
+    Make a contact sheet from a group of filenames:
+
+    fnames       A list of names of the image files
+    
+    ncols        Number of columns in the contact sheet
+    nrows        Number of rows in the contact sheet
+    photow       The width of the photo thumbs in pixels
+    photoh       The height of the photo thumbs in pixels
+
+    marl         The left margin in pixels
+    mart         The top margin in pixels
+    marr         The right margin in pixels
+    marl         The left margin in pixels
+
+    padding      The padding between images in pixels
+
+    returns a PIL image object.
+    """
+
+    # Read in all images and resize appropriately
+    imgs = [Image.open(fn).resize((photow,photoh)) for fn in fnames]
+
+    # Calculate the size of the output image, based on the
+    #  photo thumb sizes, margins, and padding
+    marw = marl+marr
+    marh = mart+ marb
+
+    padw = (ncols-1)*padding
+    padh = (nrows-1)*padding
+    isize = (ncols*photow+marw+padw,nrows*photoh+marh+padh)
+
+    # Create the new image. The background doesn't have to be white
+    white = (255,255,255)
+    inew = Image.new('RGB',isize,white)
+
+    # Insert each thumb:
+    for irow in range(nrows):
+        for icol in range(ncols):
+            left = marl + icol*(photow+padding)
+            right = left + photow
+            upper = mart + irow*(photoh+padding)
+            lower = upper + photoh
+            bbox = (left,upper,right,lower)
+            try:
+                img = imgs.pop(0)
+            except:
+                break
+            inew.paste(img,bbox)
+    return inew
+
+make_contact_sheet(["Neutral Images/1.JPG","Neutral Images/2.JPG","Neutral Images/3.JPG"],2,2,1800,1000,5,5,5,5,1).save("contact_neu.jpg")
